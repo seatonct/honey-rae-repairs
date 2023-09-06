69f65dcb7b7be63b98ab5888c3808f0cf55148b1
Charles Seaton <seatonct@gmail.com>
2023-09-06 11:25:09 -0500

    finished part 3, chapter 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { CustomerList } from "./components/customers/CustomersList";
 import { EmployeeList } from "./components/employees/EmployeesList";
 import { TicketList } from "./components/tickets/TicketList";
+import { Route, Routes, Outlet } from "react-router-dom";
+import { NavBar } from "./components/nav/NavBar";
 import "./services/App.css";
 
 export const App = () => {
   return (
-    <>
-      {/* <TicketList /> */}
-      {/* <CustomerList /> */}
-      <EmployeeList />
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <NavBar />
+            <Outlet />
+          </>
+        }
+      >
+        <Route path="tickets" element={<TicketList />} />
+        <Route path="customers" element={<CustomerList />} />
+      </Route>
+    </Routes>
   );
 };

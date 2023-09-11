c55ec47776edc9522c8cfa415804b9968fdb81e4
Charles Seaton <seatonct@gmail.com>
2023-09-11 15:57:28 -0500

    finished most of chapter 3 of part 3.

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -4,17 +4,21 @@ import { Ticket } from "./Ticket";
 import "./Tickets.css";
 import { TicketFilter } from "./TicketFilter";
 
-export const TicketList = () => {
+export const TicketList = ({ currentUser }) => {
   const [allTickets, setAllTickets] = useState([]);
   const [showEmergencyOnly, setShowEmergencyOnly] = useState(false);
   const [filteredTickets, setFilteredTickets] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
-  useEffect(() => {
+  const getAndSetTickets = () => {
     getAllTickets().then((ticketsArray) => {
       setAllTickets(ticketsArray);
       console.log("tickets set!");
     });
+  };
+
+  useEffect(() => {
+    getAndSetTickets();
   }, []);
 
   useEffect(() => {
@@ -44,7 +48,14 @@ export const TicketList = () => {
       />
       <article className="tickets">
         {filteredTickets.map((ticketObj) => {
-          return <Ticket ticket={ticketObj} key={ticketObj.id} />;
+          return (
+            <Ticket
+              ticket={ticketObj}
+              currentUser={currentUser}
+              getAndSetTickets={getAndSetTickets}
+              key={ticketObj.id}
+            />
+          );
         })}
       </article>
     </div>

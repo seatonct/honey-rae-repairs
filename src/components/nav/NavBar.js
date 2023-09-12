4a7207f548cbf3a73618af83695a448c4407d4cb
Charles Seaton <seatonct@gmail.com>
2023-09-12 15:05:38 -0500

    finished chapter 4 of part 3

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -14,6 +14,9 @@ export const NavBar = () => {
       <li className="navbar-item">
         <Link to="/employees">Employees</Link>
       </li>
+      <li className="navbar-item">
+        <Link to="/profile">Profile</Link>
+      </li>
       {localStorage.getItem("honey_user") ? (
         <li className="navbar-item navbar-logout">
           <Link
--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -9,3 +9,13 @@ export const getEmployeeByUserId = async (userId) => {
     `http://localhost:8088/employees?userId=${userId}&_expand=user`
   ).then((res) => res.json());
 };
+
+export const updateEmployee = (employee) => {
+  return fetch(`http://localhost:8088/employees/${employee.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(employee),
+  });
+};
